b94332145351f0efe0bf1323eaf8e4a530589084
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-05-01 11:14:09 +1200

    Add the integration test files

---
+++ test/integration/pester/environment/Initialize-Environment.ps1
@@ -0,0 +1,118 @@
+function Get-IpAddress {
+    $ErrorActionPreference = 'Stop'
+
+    $output = & ip a show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+
+    return $output.Trim()
+}
+
+function Initialize-Environment
+{
+    $ErrorActionPreference = 'Stop'
+
+    try
+    {
+        Start-TestConsul
+
+        Write-Output "Waiting for 10 seconds for consul and vault to start ..."
+        Start-Sleep -Seconds 10
+
+        Join-Cluster
+
+        Set-ConsulKV
+
+        Write-Output "Giving consul-template 30 seconds to process the data ..."
+        Start-Sleep -Seconds 30
+    }
+    catch
+    {
+        $currentErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = 'Continue'
+
+        try
+        {
+            Write-Error $errorRecord.Exception
+            Write-Error $errorRecord.ScriptStackTrace
+            Write-Error $errorRecord.InvocationInfo.PositionMessage
+        }
+        finally
+        {
+            $ErrorActionPreference = $currentErrorActionPreference
+        }
+
+        # rethrow the error
+        throw $_.Exception
+    }
+}
+
+function Join-Cluster
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    Write-Output "Joining the local consul ..."
+
+    # connect to the actual local consul instance
+    $ipAddress = Get-IpAddress
+    Write-Output "Joining: $($ipAddress):8351"
+
+    Start-Process -FilePath "consul" -ArgumentList "join $($ipAddress):8351"
+
+    Write-Output "Getting members for client"
+    & consul members
+
+    Write-Output "Getting members for server"
+    & consul members -http-addr=http://127.0.0.1:8550
+}
+
+function Set-ConsulKV
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    # Load config/services/consul
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/datacenter 'test-integration'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/domain 'integrationtest'
+
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/metrics/statsd/rules '\"consul.*.*.* .measurement.measurement.field\",'
+
+    # Explicitly don't provide a metrics address because that means telegraf will just send the metrics to
+    # a black hole
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/metrics/databases/system 'system'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/metrics/databases/statsd 'services'
+
+    # load config/services/queue
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/protocols/http/host 'http.queue'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/protocols/http/port '15672'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/protocols/amqp/host 'amqp.queue'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/protocols/amqp/port '5672'
+
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/logs/syslog/username 'testuser'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/logs/syslog/vhost 'testlogs'
+}
+
+function Start-TestConsul
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    if (-not (Test-Path /test/consul))
+    {
+        New-Item -Path /test/consul -ItemType Directory | Out-Null
+    }
+
+    Write-Output "Starting consul ..."
+    $process = Start-Process `
+        -FilePath "consul" `
+        -ArgumentList "agent -config-file /test/pester/environment/consul.json" `
+        -PassThru `
+        -RedirectStandardOutput /test/consul/output.out `
+        -RedirectStandardError /test/consul/error.out
+}
